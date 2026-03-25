a1d0b9be4805fb096f4febfe21438df32e87b166
ASLeonard <alexander.leonard@usys.ethz.ch>
2026-03-25 14:01:41 +0100

    clean up cmake

--- include/cpu.h
+++ include/cpu.h
@@ -2,36 +2,12 @@
 #define GCTA_CPU_H
 
 #if defined(__x86_64__) || (defined(_M_X64) && !defined(_M_ARM64EC)) || defined(__amd64)
-  #define GCTA_ARCH_x86_64 1
-#else
-  #define GCTA_ARCH_x86_64 0
-#endif
-
-#if defined(__i386__) || defined(_M_IX86) || defined(_X86_) || defined(__i386)
-  #define GCTA_ARCH_i386 1
-#else
-  #define GCTA_ARCH_i386 0
-#endif
-
-#if GCTA_ARCH_x86_64 || GCTA_ARCH_i386
   #define GCTA_CPU_x86 1
 #else
   #define GCTA_CPU_x86 0
 #endif
 
-#if defined(__arm__)
-  #define GCTA_ARCH_ARM 1
-#else
-  #define GCTA_ARCH_ARM 0
-#endif
-
 #if defined(__aarch64__) || defined(_M_ARM64) || defined(_M_ARM64EC)
-  #define GCTA_ARCH_ARM64 1
-#else
-  #define GCTA_ARCH_ARM64 0
-#endif
-
-#if GCTA_ARCH_ARM || GCTA_ARCH_ARM64
   #define GCTA_CPU_ARM 1
 #else
   #define GCTA_CPU_ARM 0
@@ -43,50 +19,45 @@
   #endif
   #include <mkl.h>
   typedef int gcta_blas_int;
-#elif defined(GCTA_USE_ACCELERATE)
-  #ifndef EIGEN_USE_BLAS
-  #define EIGEN_USE_BLAS
-  #endif
-  #include <cblas_new.h>
-  #include <lapack.h>
-  typedef __LAPACK_int gcta_blas_int;
-
-  #define dpotrf dpotrf_
-  #define dpotri dpotri_
-  #define dgetrf dgetrf_
-  #define dgetri dgetri_
-  #define dgeqrf dgeqrf_
-  #define dormqr dormqr_
 #else
   #ifndef EIGEN_USE_BLAS
   #define EIGEN_USE_BLAS
   #endif
-  #include <cblas.h>
-  #if defined(__has_include)
-    #if __has_include(<lapacke.h>)
-      #include <lapacke.h>
+
+  #if defined(GCTA_USE_ACCELERATE)
+    #include <cblas_new.h>
+    #include <lapack.h>
+    typedef __LAPACK_int gcta_blas_int;
+  #else
+    #include <cblas.h>
+    #if defined(__has_include)
+      #if __has_include(<lapacke.h>)
+        #include <lapacke.h>
+      #endif
     #endif
+    typedef int gcta_blas_int;
+
+    // Fortran LAPACK declarations for generic BLAS/LAPACK
+    extern "C" {
+      void dpotrf_(const char *uplo, const gcta_blas_int *n, double *a,
+                   const gcta_blas_int *lda, gcta_blas_int *info);
+      void dpotri_(const char *uplo, const gcta_blas_int *n, double *a,
+                   const gcta_blas_int *lda, gcta_blas_int *info);
+      void dgetrf_(const gcta_blas_int *m, const gcta_blas_int *n, double *a,
+                   const gcta_blas_int *lda, gcta_blas_int *ipiv, gcta_blas_int *info);
+      void dgetri_(const gcta_blas_int *n, double *a, const gcta_blas_int *lda,
+                   const gcta_blas_int *ipiv, double *work, const gcta_blas_int *lwork, gcta_blas_int *info);
+      void dgeqrf_(const gcta_blas_int *m, const gcta_blas_int *n, double *a,
+                   const gcta_blas_int *lda, double *tau, double *work, const gcta_blas_int *lwork, gcta_blas_int *info);
+      void dormqr_(const char *side, const char *trans, const gcta_blas_int *m,
+                   const gcta_blas_int *n, const gcta_blas_int *k, const double *a,
+                   const gcta_blas_int *lda, const double *tau, double *c, const gcta_blas_int *ldc,
+                   double *work, const gcta_blas_int *lwork, gcta_blas_int *info);
+    }
   #endif
-  typedef int gcta_blas_int;
 
-  // Fortran LAPACK declarations for generic BLAS/LAPACK
-  extern "C" {
-    void dpotrf_(const char *uplo, const gcta_blas_int *n, double *a,
-                 const gcta_blas_int *lda, gcta_blas_int *info);
-    void dpotri_(const char *uplo, const gcta_blas_int *n, double *a,
-                 const gcta_blas_int *lda, gcta_blas_int *info);
-    void dgetrf_(const gcta_blas_int *m, const gcta_blas_int *n, double *a,
-                 const gcta_blas_int *lda, gcta_blas_int *ipiv, gcta_blas_int *info);
-    void dgetri_(const gcta_blas_int *n, double *a, const gcta_blas_int *lda,
-                 const gcta_blas_int *ipiv, double *work, const gcta_blas_int *lwork, gcta_blas_int *info);
-    void dgeqrf_(const gcta_blas_int *m, const gcta_blas_int *n, double *a,
-                 const gcta_blas_int *lda, double *tau, double *work, const gcta_blas_int *lwork, gcta_blas_int *info);
-    void dormqr_(const char *side, const char *trans, const gcta_blas_int *m,
-                 const gcta_blas_int *n, const gcta_blas_int *k, const double *a,
-                 const gcta_blas_int *lda, const double *tau, double *c, const gcta_blas_int *ldc,
-                 double *work, const gcta_blas_int *lwork, gcta_blas_int *info);
-  }
-
+  // Alias underscore-suffixed Fortran names to the bare names used in this codebase.
+  // Must come after includes so Accelerate's <lapack.h> can set up its own ILP64 aliases first.
   #define dpotrf dpotrf_
   #define dpotri dpotri_
   #define dgetrf dgetrf_
