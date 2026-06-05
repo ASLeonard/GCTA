1220dfce3fcf039198fe57f50ba64ffe9aa8866e
ASLeonard <alexander.leonard@usys.ethz.ch>
2026-06-05 08:54:52 +0200

    more cross-arch compatability

--- include/cpu.h
+++ include/cpu.h
@@ -42,6 +42,16 @@
   #include <veclib/cblas_new.h>
   #include <veclib/lapack.h>
   typedef __LAPACK_int gcta_blas_int;
+
+    extern "C" {
+    void dsyrk_(const char* uplo, const char* trans, const int* n, const int* k,
+                const double* alpha, const double* a, const int* lda,
+                const double* beta, double* c, const int* ldc);
+    void dgemm_(const char* transa, const char* transb, const int* m, const int* n, const int* k,
+                const double* alpha, const double* a, const int* lda,
+                const double* b, const int* ldb, const double* beta,
+                double* c, const int* ldc);
+  }
 #endif
 
 // Portable dpotrf: Cholesky factorization of a symmetric positive-definite matrix
